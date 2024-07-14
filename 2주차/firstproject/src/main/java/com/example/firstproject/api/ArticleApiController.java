07ca61581cab16036912e656b5aba2364cabbdd8
h22kyung <95400025+h22kyung@users.noreply.github.com>
2024-07-15 01:04:10 +0900

    Add files via upload

---
+++ 2주차/firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -0,0 +1,20 @@
+package com.example.firstproject.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArticleApiController {
+    //GET
+  //  @GetMapping("/api/articles")
+  //  public List<Article> index(){
+   //     return articleRepository.findAll();
+        }
+    //POST
+    //PUT
+    //DELETE
+
+
+
+
+
+//}
