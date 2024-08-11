a402d8de6137bac2b05a930fed4cb44d69b2dc2e
h22kyung <95400025+h22kyung@users.noreply.github.com>
2024-08-12 00:55:48 +0900

    Add files via upload

---
+++ 5주차/firstproject/src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -0,0 +1,49 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/articles")
+public class ArticleApiController {
+        @Autowired
+        private ArticleRepository articleRepository;
+        @Autowired
+        private ArticleService articleService;
+
+        //GET
+        @GetMapping("/get")
+        public List<Article> getAllArticles() {
+                return articleService.getAllArticles();
+        }
+
+        @GetMapping("/get/{id}")
+        public Optional<Article> getArticleById(@PathVariable Long id) {
+
+                return articleService.getArticleById(id);
+        }
+
+        //POST
+        @PostMapping
+        public Article createArticle(@RequestBody Article article) {
+                return articleService.createArticle(article);
+        }
+
+        //PUT
+        /*@PutMapping("/put/{id}")
+        public Article updateArticle(@PathVariable Long id, @RequestBody Article articleDetails) {
+                return articleService.updateArticle(id, articleDetails);
+        }*/
+        //DELETe
+        @DeleteMapping("/delete/{id}")
+        public void deleteArticle(@PathVariable Long id) {
+                articleService.deleteArticle(id);
+        }
+}
+
---
+++ 5주차/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,61 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+//이 클래스는 엔티티 이다!!
+//테이블은 클래스 이름과 동일하게 생성된다 ..
+@Entity
+public class Article {
+    @Id //엔티티의 대푯값 지정
+    @GeneratedValue //자동으로 매겨짐
+    private Long id;
+    @Column //db 테이블의 title 열과 연결되도록 ..
+    private String title;
+    @Column
+    private String content;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+  /*  public Article(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+
+    }*/
+}
